4020601a93d2b3cb76fd8bfa5b3068238a027cc6
Munnaraj98 <munnaraj740800@gmail.com>
2025-01-15 03:35:15 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,18 @@
+
+
+# Insert your Telegram bot token here
+bot = telebot.TeleBot('7658787198:AAFlRySs2vBPJ-LVE2WTHviF8wG7IpUj4S8')
+
+# Admin user IDs
+admin_id = ["7855020275"]
+
+# Group and channel details
+GROUP_ID = "-1002374071862"
+CHANNEL_USERNAME = "@rajaraj_0"
+
+
+
+
+
+
+  
