6c22b125bdc44ba693396dda66c7bcd779b169e5
Munnaraj98 <munnaraj740800@gmail.com>
2025-01-15 03:37:48 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -1,14 +1,11 @@
 
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7658787198:AAFlRySs2vBPJ-LVE2WTHviF8wG7IpUj4S8')
+bot = telebot.TeleBot('7784344456:AAGpbtyhKOBrAyJbwOXao6GznMP3yaQ_fBQ')
 
 # Admin user IDs
 admin_id = ["7855020275"]
 
-# Group and channel details
-GROUP_ID = "-1002374071862"
-CHANNEL_USERNAME = "@rajaraj_0"
 
 
 
